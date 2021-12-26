d3e0165fc4b4d84932a13f229b6c4a5664f35446
Alexander <alexander.s.trushnikov@gmail.com>
2021-12-26 23:48:43 +0500

    Added path to homepage.

--- retailweb/retailweb/urls.py
+++ retailweb/retailweb/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('homepage/', include('homepage.urls')),
     path('admin/', admin.site.urls),
 ]
