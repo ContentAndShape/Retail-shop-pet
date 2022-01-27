e35b94b719634476f009cf43afc163ab559e38fa
unknown <alexandr.s.trushnikov@gmail.com>
2022-01-27 22:08:56 +0300

    added selected product mapping

--- retailweb/products/urls.py
+++ retailweb/products/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.categories, name='categories'),
     path('<slug:product_category>/', views.index),
+    path('<slug:product_category>/<int:selected_prod_id>', views.selected),
 ]
--- retailweb/products/views.py
+++ retailweb/products/views.py
@@ -8,21 +8,32 @@ def categories(request):
 
 
 def index(request, product_category):
-    products_list = Product.objects.filter(category=product_category)
-    grouped_products_list = group_by_two(products_list)
+    products_query = Product.objects.filter(
+        category=product_category).order_by('-id') # newest models first
+    grouped_products_list = group_by_four(products_query)
     context = {
         'grouped_products_list': grouped_products_list,
         'product_category': product_category,
     }
     return render(request, 'products.html', context)
 
+
+def selected(request, product_category, selected_prod_id):
+    product_query = Product.objects.get(id=selected_prod_id)
+    context = {
+        'selected_product': product_query,
+        'product_category': product_category,
+    }
+    return render(request, 'selected_product.html', context)
+
 # Returns the list of tuples from the iterable argument
-def group_by_two(iter):
+def group_by_four(iter):
     iter = list(iter)
 
     try:
-        return [(iter[i], iter[i + 1]) for i in range(0, len(iter), 2)]
-    # If the number of elements is odd:
+        return [(iter[i], iter[i + 1], iter[i + 2], iter[i + 3]) for i 
+            in range(0, len(iter), 4)]
+    # If the number of elements < 4:
     except IndexError:
         iter.append(None)
-        return [(iter[i], iter[i + 1]) for i in range(0, len(iter), 2)]
+        return group_by_four(iter)
